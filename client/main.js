c6221f3421a9d080ee16d1d4ad67eefacacd2d20
Unknown <julien.luccioni@protonmail.com>
2019-04-04 18:58:31 +0200

    fetch&cie done

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+let RATES;
+
 async function installServiceWorkerAsync() {
   if ("serviceWorker" in navigator) {
     try {
@@ -10,12 +12,45 @@ async function installServiceWorkerAsync() {
   }
 }
 
-async function fetchAsync(url = API_MANGA) {
-  console.log(url);
+function onLoad() {
+  installServiceWorkerAsync();
+  addRateInfoToTable();
+}
+
+function convert() {
+  const selectedItem = document.getElementById("selectCurrency").value;
+  const rate = RATES[selectedItem];
+  const valueToConvert = parseFloat(
+    document.getElementById("valueToConvert").value
+  );
+  document.getElementById("convertedValue").value = valueToConvert * rate;
+}
+
+async function addRateInfoToTable() {
+  const { rates } = await fetchAsync("https://localhost:5001/api/xeu");
+  RATES = rates;
+  const html = Object.entries(rates).reduce((accumulator, [key, value]) => {
+    setOption(key);
+    return `${accumulator}
+       <tr>
+        <th>${key}</th>
+        <td>${value}</td>
+       </tr>`;
+  }, "");
+  document.getElementById("bodyTableRate").innerHTML = html;
+}
+
+function setOption(key) {
+  const select = document.getElementById("selectCurrency");
+  const option = document.createElement("option");
+  option.text = key;
+  select.appendChild(option);
+}
+
+async function fetchAsync(url) {
   const res = await fetch(url);
   if (!res.ok) {
     throw "ERROR";
   }
-  const json = await res.json();
-  return json;
+  return res.json();
 }
